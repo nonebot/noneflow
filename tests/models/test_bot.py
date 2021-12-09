101af3e847e50c7b34a33ee8a52edacf0e922287
hemengyang <hmy0119@gmail.com>
2021-12-09 22:08:46 +0800

    测试提取信息

--- tests/models/test_bot.py
+++ tests/models/test_bot.py
@@ -1,5 +1,8 @@
 from collections import OrderedDict
 
+from github.Issue import Issue
+from pytest_mock import MockerFixture
+
 from src.models import BotPublishInfo
 
 
@@ -12,6 +15,33 @@ def test_bot_info() -> None:
         tags=["tag"],
         is_official=False,
     )
+
+    assert OrderedDict(info.dict()) == OrderedDict(
+        name="name",
+        desc="desc",
+        author="author",
+        homepage="https://www.baidu.com",
+        tags=["tag"],
+        is_official=False,
+    )
+
+
+def test_bot_from_issue(mocker: MockerFixture) -> None:
+    body = """
+        <!-- DO NOT EDIT ! -->
+        <!--
+        - name: name
+        - desc: desc
+        - homepage: https://www.baidu.com
+        - tags: tag
+        - is_official: false
+        -->
+        """
+    mock_issue: Issue = mocker.MagicMock()  # type: ignore
+    mock_issue.body = body  # type: ignore
+    mock_issue.user.login = "author"  # type: ignore
+
+    info = BotPublishInfo.from_issue(mock_issue)
 
     assert OrderedDict(info.dict()) == OrderedDict(
         name="name",
