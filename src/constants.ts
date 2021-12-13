99e5707143e43d6153295e74198ad6b7398eceae
StarHeart <starheart233@gmail.com>
2021-12-13 09:38:55 +0800

    chore: correct grammar

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const commentTitle = '# 📃 Publish Check Result'
 
 export const reuseMessage =
-  ':recycle: This comment has been updated with latest result.'
+  ':recycle: This comment has been updated with the latest result.'
 
 export const poweredByBotMessage = '💪 Powered by NoneBot2 Publish Bot'
