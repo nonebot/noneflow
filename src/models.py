71f4f8f23c1679160bd1db694f27764bedc72abc
hemengyang <hmy0119@gmail.com>
2022-11-14 19:43:47 +0800

    fix: runner_debug 可为空

--- src/models.py
+++ src/models.py
@@ -112,7 +112,7 @@ class Settings(BaseSettings):
     github_repository: str
     github_event_name: Optional[str] = None
     github_event_path: Path
-    runner_debug: bool
+    runner_debug: Optional[bool]
 
 
 class PublishType(Enum):
