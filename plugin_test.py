16cba2182a83333f25933f2d7cfc63df7c3699e4
uy/sun <hmy0119@gmail.com>
2023-03-16 16:06:50 +0800

    feat: 测试插件是否在 import 前使用 require 声明依赖 (#115)

--- plugin_test.py
+++ plugin_test.py
@@ -8,6 +8,7 @@ import os
 import re
 from asyncio import create_subprocess_shell, run, subprocess
 from pathlib import Path
+from urllib.request import urlopen
 
 # 匹配信息的正则表达式
 ISSUE_PATTERN = r"\*\*{}：\*\*\s+([^\s*].*?)(?=(?:\s+\*\*|$))"
@@ -15,15 +16,27 @@ ISSUE_PATTERN = r"\*\*{}：\*\*\s+([^\s*].*?)(?=(?:\s+\*\*|$))"
 PROJECT_LINK_PATTERN = re.compile(ISSUE_PATTERN.format("PyPI 项目名"))
 MODULE_NAME_PATTERN = re.compile(ISSUE_PATTERN.format("插件 import 包名"))
 
-RUNNER = """from nonebot import init, load_plugin
+RUNNER = """from nonebot import init, load_plugin, require
 
 init(driver="~none")
 valid = load_plugin("{}")
+{}
 if not valid:
     exit(1)
 else:
     exit(0)
 """
+
+
+def get_plugin_list() -> dict[str, str]:
+    """获取插件列表
+
+    通过 package_name 获取 module_name
+    """
+    with urlopen("https://v2.nonebot.dev/plugins.json") as response:
+        plugins = json.loads(response.read())
+
+    return {plugin["project_link"]: plugin["module_name"] for plugin in plugins}
 
 
 class PluginTest:
@@ -35,8 +48,11 @@ class PluginTest:
 
         self._create = False
         self._run = False
+        self._deps = []
 
         self._output_lines: list[str] = []
+
+        self._plugin_list = get_plugin_list()
 
     async def run(self):
         await self.create_poetry_project()
@@ -66,11 +82,11 @@ class PluginTest:
             if self._create:
                 print(f"项目 {self.project_link} 创建成功。")
             else:
-                self.log_output(f"项目 {self.project_link} 创建失败：")
+                self._log_output(f"项目 {self.project_link} 创建失败：")
                 for i in stderr.decode().strip().splitlines():
-                    self.log_output(f"    {i}")
+                    self._log_output(f"    {i}")
         else:
-            self.log_output(f"项目 {self.project_link} 已存在，跳过创建。")
+            self._log_output(f"项目 {self.project_link} 已存在，跳过创建。")
             self._create = True
 
     async def show_package_info(self) -> None:
@@ -83,16 +99,40 @@ class PluginTest:
             stdout, _ = await proc.communicate()
             code = proc.returncode
             if not code:
-                self.log_output(f"插件 {self.project_link} 的信息如下：")
+                self._log_output(f"插件 {self.project_link} 的信息如下：")
                 for i in stdout.decode().strip().splitlines():
-                    self.log_output(f"    {i}")
+                    self._log_output(f"    {i}")
             else:
-                self.log_output(f"插件 {self.project_link} 信息获取失败。")
+                self._log_output(f"插件 {self.project_link} 信息获取失败。")
+
+    async def show_plugin_dependencies(self) -> None:
+        if self._path.exists():
+            proc = await create_subprocess_shell(
+                f"cd {self._path.resolve()} && poetry export --without-hashes",
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            stdout, _ = await proc.communicate()
+            code = proc.returncode
+            if not code:
+                self._log_output(f"插件 {self.project_link} 依赖的插件如下：")
+                for i in stdout.decode().strip().splitlines():
+                    module_name = self._get_plugin_module_name(i)
+                    if module_name:
+                        self._deps.append(module_name)
+                self._log_output(f"    {', '.join(self._deps)}")
+            else:
+                self._log_output(f"插件 {self.project_link} 依赖获取失败。")
 
     async def run_poetry_project(self) -> None:
         if self._path.exists():
             with open(self._path / "runner.py", "w") as f:
-                f.write(RUNNER.format(self.module_name))
+                f.write(
+                    RUNNER.format(
+                        self.module_name,
+                        "\n".join([f"require('{i}')" for i in self._deps]),
+                    )
+                )
 
             proc = await create_subprocess_shell(
                 f"cd {self._path.resolve()} && poetry run python runner.py",
@@ -104,22 +144,30 @@ class PluginTest:
 
             self._run = not code
             if self._run:
-                self.log_output(f"插件 {self.module_name} 加载正常。")
+                self._log_output(f"插件 {self.module_name} 加载正常。")
             else:
-                self.log_output(f"插件 {self.module_name} 加载出错：")
-                _err = stderr.decode().strip()
-                if len(_err.splitlines()) > 1:
-                    for i in _err.splitlines():
-                        self.log_output(f"    {i}")
-                elif not _err:
-                    self.log_output(stdout.decode().strip())
-                else:
-                    self.log_output(_err)
+                self._log_output(f"插件 {self.module_name} 加载出错：")
+                _out = stdout.decode().strip().splitlines()
+                _err = stderr.decode().strip().splitlines()
+                for i in _out:
+                    self._log_output(f"    {i}")
+                for i in _err:
+                    self._log_output(f"    {i}")
 
-    def log_output(self, output: str) -> None:
+    def _log_output(self, output: str) -> None:
         """记录输出，同时打印到控制台"""
         print(output)
         self._output_lines.append(output)
+
+    def _get_plugin_module_name(self, require: str) -> str | None:
+        # anyio==3.6.2 ; python_version >= "3.11" and python_version < "4.0"
+        # pydantic[dotenv]==1.10.6 ; python_version >= "3.10" and python_version < "4.0"
+        match = re.match(r"^(.+?)(?:\[.+\])?==", require.strip())
+        if match:
+            package_name = match.group(1)
+            # 不用包括自己
+            if package_name in self._plugin_list and package_name != self.project_link:
+                return self._plugin_list[package_name]
 
 
 def main():
@@ -128,7 +176,7 @@ def main():
         print("未找到 GITHUB_EVENT_PATH，已跳过")
         return
 
-    with open(event_path) as f:
+    with open(event_path, encoding="utf8") as f:
         event = json.load(f)
 
     event_name = os.environ.get("GITHUB_EVENT_NAME")
