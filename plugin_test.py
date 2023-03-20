4274738639db745ffb91464ec3ddbb05d85fb140
uy/sun <hmy0119@gmail.com>
2023-03-20 23:00:56 +0800

    feat: 插件测试添加作业摘要 (#116)

--- plugin_test.py
+++ plugin_test.py
@@ -3,6 +3,7 @@
 主要测试代码来自 https://github.com/Lancercmd/nonebot2-store-test
 """
 
+import html
 import json
 import os
 import re
@@ -68,6 +69,13 @@ class PluginTest:
         output = "\n".join(self._output_lines)
         with open(os.environ["GITHUB_OUTPUT"], "a") as f:
             f.write(f"OUTPUT<<EOF\n{output}\nEOF\n")
+        # 输出至作业摘要
+        with open(os.environ["GITHUB_STEP_SUMMARY"], "a") as f:
+            summary = f"插件 {self.project_link} 测试结果：{'成功' if self._run else '失败'}"
+            summary += (
+                f"<details><summary>测试输出</summary>{html.escape(output)}</details>"
+            )
+            f.write(f"{summary}\n")
 
     async def create_poetry_project(self) -> None:
         if not self._path.exists():
