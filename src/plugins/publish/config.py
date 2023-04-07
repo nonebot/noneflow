2af45712b0fae62924694fb14c4aa68b70e1b099
hemengyang <hmy0119@gmail.com>
2023-04-07 20:52:58 +0800

    fix: 处理 plugin_test_result 为空字符串的情况

--- src/plugins/publish/config.py
+++ src/plugins/publish/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from nonebot import get_driver
-from pydantic import BaseModel, Extra
+from pydantic import BaseModel, Extra, validator
 
 
 class PublishConfig(BaseModel):
@@ -18,6 +18,14 @@ class Config(BaseModel, extra=Extra.ignore):
     skip_plugin_test: bool = False
     plugin_test_result: bool = False
     plugin_test_output: str = ""
+
+    @validator("plugin_test_result", pre=True)
+    def plugin_test_result_validator(cls, v):
+        # 如果插件测试没有运行时，会得到一个空字符串
+        # 这里将其转换为布尔值，不然会报错
+        if v == "":
+            return False
+        return v
 
 
 plugin_config = Config.parse_obj(get_driver().config)
