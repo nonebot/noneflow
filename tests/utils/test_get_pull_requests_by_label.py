3b3dd3db8107ca8fa4b349847252d79af2168c79
hemengyang <hmy0119@gmail.com>
2023-04-08 14:17:51 +0800

    重新加回测试

--- tests/utils/test_get_pull_requests_by_label.py
+++ tests/utils/test_get_pull_requests_by_label.py
@@ -1,47 +1,79 @@
-# from pytest_mock import MockerFixture
+from typing import cast
+
+from nonebot import get_adapter
+from nonebot.adapters.github import Adapter, GitHubBot
+from nonebot.adapters.github.config import GitHubApp
+from nonebug import App
+from pytest_mock import MockerFixture
 
 
-# def test_get_pull_requests_by_label(mocker: MockerFixture) -> None:
-#     """测试获取指定标签的拉取请求"""
-#     from src import Bot
+async def test_get_pull_requests_by_label(app: App, mocker: MockerFixture) -> None:
+    """测试获取指定标签的拉取请求"""
+    from src.plugins.publish.depends import get_pull_requests_by_label
+    from src.plugins.publish.models import PublishType, RepoInfo
 
-#     bot = Bot()
-#     bot.github = mocker.MagicMock()
+    mock_label = mocker.MagicMock()
+    mock_label.name = "Bot"
 
-#     mock_label = mocker.MagicMock()
-#     mock_label.name = "Bot"
+    mock_pull = mocker.MagicMock()
+    mock_pull.labels = [mock_label]
 
-#     mock_pull = mocker.MagicMock()
-#     mock_pull.labels = [mock_label]
+    mock_pulls_resp = mocker.MagicMock()
+    mock_pulls_resp.parsed_data = [mock_pull]
 
-#     mock_pulls_resp = mocker.MagicMock()
-#     mock_pulls_resp.parsed_data = [mock_pull]
-#     bot.github.rest.pulls.list.return_value = mock_pulls_resp
+    async with app.test_api() as ctx:
+        adapter = get_adapter(Adapter)
+        bot = ctx.create_bot(
+            base=GitHubBot,
+            adapter=adapter,
+            self_id=GitHubApp(app_id="1", private_key="1"),  # type: ignore
+        )
+        bot = cast(GitHubBot, bot)
 
-#     pulls = bot.get_pull_requests_by_label("Bot")
+        ctx.should_call_api(
+            "rest.pulls.async_list",
+            {"owner": "owner", "repo": "repo", "state": "open"},
+            mock_pulls_resp,
+        )
 
-#     bot.github.rest.pulls.list.assert_called_with("owner", "repo", state="open")
-#     assert pulls[0] == mock_pull
+        pulls = await get_pull_requests_by_label(
+            bot, RepoInfo(owner="owner", repo="repo"), PublishType.BOT
+        )
+        assert pulls[0] == mock_pull
 
 
-# def test_get_pull_requests_by_label_not_match(mocker: MockerFixture) -> None:
-#     """测试获取指定标签的拉取请求，但是没有匹配的"""
-#     from src import Bot
+async def test_get_pull_requests_by_label_not_match(
+    app: App, mocker: MockerFixture
+) -> None:
+    """测试获取指定标签的拉取请求，但是没有匹配的"""
+    from src.plugins.publish.depends import get_pull_requests_by_label
+    from src.plugins.publish.models import PublishType, RepoInfo
 
-#     bot = Bot()
-#     bot.github = mocker.MagicMock()
+    mock_label = mocker.MagicMock()
+    mock_label.name = "Some"
 
-#     mock_label = mocker.MagicMock()
-#     mock_label.name = "Some"
+    mock_pull = mocker.MagicMock()
+    mock_pull.labels = [mock_label]
 
-#     mock_pull = mocker.MagicMock()
-#     mock_pull.labels = [mock_label]
+    mock_pulls_resp = mocker.MagicMock()
+    mock_pulls_resp.parsed_data = [mock_pull]
 
-#     mock_pulls_resp = mocker.MagicMock()
-#     mock_pulls_resp.parsed_data = [mock_pull]
-#     bot.github.rest.pulls.list.return_value = mock_pulls_resp
+    async with app.test_api() as ctx:
+        adapter = get_adapter(Adapter)
+        bot = ctx.create_bot(
+            base=GitHubBot,
+            adapter=adapter,
+            self_id=GitHubApp(app_id="1", private_key="1"),  # type: ignore
+        )
+        bot = cast(GitHubBot, bot)
 
-#     pulls = bot.get_pull_requests_by_label("Bot")
+        ctx.should_call_api(
+            "rest.pulls.async_list",
+            {"owner": "owner", "repo": "repo", "state": "open"},
+            mock_pulls_resp,
+        )
 
-#     bot.github.rest.pulls.list.assert_called_with("owner", "repo", state="open")
-#     assert pulls == []
+        pulls = await get_pull_requests_by_label(
+            bot, RepoInfo(owner="owner", repo="repo"), PublishType.BOT
+        )
+        assert pulls == []
