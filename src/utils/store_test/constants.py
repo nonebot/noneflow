c0c9af842920f15eee34f08f7e5fbc2389c91a19
uy_sun <hmy0119@gmail.com>
2023-06-22 20:41:09 +0800

    change: 调整了商店测试结果存放位置

--- src/utils/store_test/constants.py
+++ src/utils/store_test/constants.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
 PLUGIN_CONFIGS_URL = "https://raw.githubusercontent.com/he0119/nonebot-store-test/main/inputs/configs.json"
-RESULTS_URL = "https://raw.githubusercontent.com/he0119/nonebot-store-test/main/results/results.json"
+RESULTS_URL = (
+    "https://raw.githubusercontent.com/he0119/nonebot-store-test/results/results.json"
+)
 
 TEST_DIR = Path("plugin_test")
 RESULTS_PATH = TEST_DIR / "results.json"
