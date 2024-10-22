9ff8739d0275310a2e86f454c6772a144042abd1
uy_sun <hmy0119@gmail.com>
2024-10-22 20:52:17 +0800

    feat: 从环境变量中读取 REGISTRY_BASE_URL 和 STORE_BASE_URL
    方便在测试环境中测试脚本

--- src/providers/constants.py
+++ src/providers/constants.py
@@ -1,13 +1,21 @@
 # NoneBot 插件商店测试结果
 # https://github.com/nonebot/registry/tree/results
-REGISTRY_BASE_URL = "https://raw.githubusercontent.com/nonebot/registry/results"
+import os
+
+REGISTRY_BASE_URL = (
+    os.environ.get("REGISTRY_BASE_URL")
+    or "https://raw.githubusercontent.com/nonebot/registry/results"
+)
 REGISTRY_RESULTS_URL = f"{REGISTRY_BASE_URL}/results.json"
 REGISTRY_PLUGINS_URL = f"{REGISTRY_BASE_URL}/plugins.json"
 REGISTRY_PLUGIN_CONFIG_URL = f"{REGISTRY_BASE_URL}/plugin_configs.json"
 
 # NoneBot 插件商店
 # https://github.com/nonebot/nonebot2/tree/master/assets
-STORE_BASE_URL = "https://raw.githubusercontent.com/nonebot/nonebot2/master/assets"
+STORE_BASE_URL = (
+    os.environ.get("STORE_BASE_URL")
+    or "https://raw.githubusercontent.com/nonebot/nonebot2/master/assets"
+)
 STORE_ADAPTERS_URL = f"{STORE_BASE_URL}/adapters.json"
 STORE_BOTS_URL = f"{STORE_BASE_URL}/bots.json"
 STORE_DRIVERS_URL = f"{STORE_BASE_URL}/drivers.json"
