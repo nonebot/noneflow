bb7f59346bf58198ed80640a3397be87b533b11e
uy/sun <hmy0119@gmail.com>
2024-10-23 23:06:17 +0800

    feat: 从环境变量中读取测试相关变量 (#236)
    * docs(examples): 简化 noneflow 的写法
    
    不再需要单独的插件测试 job
    
    * feat: 从环境变量中读取 REGISTRY_BASE_URL 和 STORE_BASE_URL
    
    方便在测试环境中测试脚本
    
    * docs(examples): 更新运行 store_test 的方法
    
    * feat: 从环境变量中读取 DOCKER_IMAGES_VERSION
    
    * chore: 添加并调整注释的位置
    
    * ci: docker tags 使用 prefix 来添加 python 版本
    
    * ci: docker tags 使用 flavor 来添加 prefix

--- src/providers/constants.py
+++ src/providers/constants.py
@@ -1,17 +1,28 @@
+import os
+
 # NoneBot 插件商店测试结果
 # https://github.com/nonebot/registry/tree/results
-REGISTRY_BASE_URL = "https://raw.githubusercontent.com/nonebot/registry/results"
+REGISTRY_BASE_URL = (
+    os.environ.get("REGISTRY_BASE_URL")
+    or "https://raw.githubusercontent.com/nonebot/registry/results"
+)
 REGISTRY_RESULTS_URL = f"{REGISTRY_BASE_URL}/results.json"
 REGISTRY_PLUGINS_URL = f"{REGISTRY_BASE_URL}/plugins.json"
 REGISTRY_PLUGIN_CONFIG_URL = f"{REGISTRY_BASE_URL}/plugin_configs.json"
 
 # NoneBot 插件商店
 # https://github.com/nonebot/nonebot2/tree/master/assets
-STORE_BASE_URL = "https://raw.githubusercontent.com/nonebot/nonebot2/master/assets"
+STORE_BASE_URL = (
+    os.environ.get("STORE_BASE_URL")
+    or "https://raw.githubusercontent.com/nonebot/nonebot2/master/assets"
+)
 STORE_ADAPTERS_URL = f"{STORE_BASE_URL}/adapters.json"
 STORE_BOTS_URL = f"{STORE_BASE_URL}/bots.json"
 STORE_DRIVERS_URL = f"{STORE_BASE_URL}/drivers.json"
 STORE_PLUGINS_URL = f"{STORE_BASE_URL}/plugins.json"
 """plugin_test.py 中也有一个常量，需要同时修改"""
 
-DOCKER_IMAGES = "ghcr.io/nonebot/nonetest:{}-main"
+# 商店测试镜像
+# https://github.com/orgs/nonebot/packages/container/package/nonetest
+DOCKER_IMAGES_VERSION = os.environ.get("DOCKER_IMAGES_VERSION") or "latest"
+DOCKER_IMAGES = f"ghcr.io/nonebot/nonetest:{{}}-{DOCKER_IMAGES_VERSION}"
