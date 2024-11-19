f14257060c01ca5a568741257e1e679a88987af3
uy_sun <hmy0119@gmail.com>
2024-11-19 22:31:14 +0800

    test: 添加测试

--- tests/github/publish/utils/test_validate_info_from_issue.py
+++ tests/github/publish/utils/test_validate_info_from_issue.py
@@ -1,4 +1,5 @@
 from githubkit.rest import Issue
+from inline_snapshot import snapshot
 from nonebug import App
 from pytest_mock import MockerFixture
 from respx import MockRouter
@@ -6,6 +7,7 @@ from respx import MockRouter
 from tests.github.utils import (
     generate_issue_body_adapter,
     generate_issue_body_bot,
+    generate_issue_body_plugin,
     generate_issue_body_plugin_skip_test,
     get_github_bot,
 )
@@ -48,6 +50,71 @@ async def test_validate_info_from_issue_bot(
 async def test_validate_info_from_issue_plugin(
     app: App, mocker: MockerFixture, mocked_api: MockRouter
 ):
+    from src.plugins.github import plugin_config
+    from src.plugins.github.models import RepoInfo
+    from src.plugins.github.models.issue import IssueHandler
+    from src.plugins.github.plugins.publish.validation import (
+        validate_plugin_info_from_issue,
+    )
+    from src.providers.docker_test import Metadata
+
+    mock_user = mocker.MagicMock()
+    mock_user.login = "test"
+    mock_user.id = 1
+
+    mock_issue = mocker.MagicMock(spec=Issue)
+    mock_issue.body = generate_issue_body_plugin()
+    mock_issue.number = 1
+    mock_issue.user = mock_user
+
+    mock_list_comments_resp = mocker.MagicMock()
+    mock_list_comments_resp.parsed_data = []
+
+    mock_test_result = mocker.MagicMock()
+    mock_test_result.metadata = Metadata(
+        name="name",
+        desc="desc",
+        homepage="https://nonebot.dev",
+        type="application",
+        supported_adapters=["~onebot.v11"],
+    )
+    mock_test_result.version = "1.0.0"
+    mock_test_result.load = True
+    mock_test_result.outputs = ['require("nonebot_plugin_alconna")', "test"]
+    mock_docker = mocker.patch("src.providers.docker_test.DockerPluginTest.run")
+    mock_docker.return_value = mock_test_result
+
+    async with app.test_api() as ctx:
+        _, bot = get_github_bot(ctx)
+        handler = IssueHandler(
+            bot=bot, repo_info=RepoInfo(owner="owner", repo="repo"), issue=mock_issue
+        )
+
+        ctx.should_call_api(
+            "rest.issues.async_list_comments",
+            {"owner": "owner", "repo": "repo", "issue_number": 1},
+            mock_list_comments_resp,
+        )
+
+        result = await validate_plugin_info_from_issue(handler)
+
+    assert result.valid
+    assert mocked_api["homepage"].called
+    assert plugin_config.github_step_summary.read_text() == snapshot(
+        """\
+插件 project_link(1.0.0) 加载成功，运行开始
+插件元数据：name='name' desc='desc' homepage='https://nonebot.dev' type='application' supported_adapters=['~onebot.v11']
+插件测试输出：
+require("nonebot_plugin_alconna")
+test
+"""
+    )
+
+
+async def test_validate_info_from_issue_plugin_skip_test(
+    app: App, mocker: MockerFixture, mocked_api: MockRouter
+):
+    """跳过插件测试的情况"""
     from src.plugins.github.models import RepoInfo
     from src.plugins.github.models.issue import IssueHandler
     from src.plugins.github.plugins.publish.validation import (
