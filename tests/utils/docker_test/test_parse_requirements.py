a48f0d62104abe07c7d9c04a124a507870db6a8b
uy_sun <hmy0119@gmail.com>
2024-11-22 09:12:12 +0800

    test: 完善解析依赖的测试

--- tests/utils/docker_test/test_parse_requirements.py
+++ tests/utils/docker_test/test_parse_requirements.py
@@ -3,7 +3,8 @@ def test_parse_requirements():
     from src.providers.docker_test.plugin_test import parse_requirements
 
     output = """
-nonebot2==2.4.0 ; python_version >= "3.9" and python_version < "4.0"
+anyio==4.6.2.post1 ; python_version >= "3.9" and python_version < "4.0"
+nonebot2[httpx]==2.4.0 ; python_version >= "3.9" and python_version < "4.0"
 nonebug==0.4.2 ; python_version >= "3.9" and python_version < "4.0"
 pydantic-core==2.27.0 ; python_version >= "3.9" and python_version < "4.0"
 pydantic==2.10.0 ; python_version >= "3.9" and python_version < "4.0"
@@ -12,6 +13,7 @@ pydantic==2.10.0 ; python_version >= "3.9" and python_version < "4.0"
     requirements = parse_requirements(output)
 
     assert requirements == {
+        "anyio": "4.6.2.post1",
         "nonebot2": "2.4.0",
         "nonebug": "0.4.2",
         "pydantic-core": "2.27.0",
