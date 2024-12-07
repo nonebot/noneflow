b6b66db4ce97a452d26d68f3b93f38f813c8f132
uy_sun <hmy0119@gmail.com>
2024-12-07 18:43:05 +0800

    test: 尝试简化 should_call_api

---
+++ tests/plugins/github/resolve/test_resolve_pull_request.py
@@ -0,0 +1,122 @@
+from typing import Any, NotRequired, TypedDict
+from unittest.mock import MagicMock
+
+from inline_snapshot import snapshot
+from nonebot.adapters.github import PullRequestClosed
+from nonebug import App
+from nonebug.mixin.process import MatcherContext
+from pytest_mock import MockerFixture
+
+from tests.plugins.github.event import get_mock_event
+from tests.plugins.github.resolve.utils import get_pr_labels
+from tests.plugins.github.utils import (
+    MockIssue,
+    generate_issue_body_remove,
+    get_github_bot,
+)
+
+
+class GitHubApi(TypedDict):
+    api: str
+    result: Any | None
+    exception: NotRequired[Exception | None]
+
+
+def should_call_api(ctx: MatcherContext, apis: list[GitHubApi], data: Any) -> None:
+    for api in apis:
+        ctx.should_call_api(**api, data=data[api["api"]])
+
+
+async def test_resolve_pull_request(
+    app: App, mocker: MockerFixture, mock_installation: MagicMock
+) -> None:
+    """删除流程的拉取请求关闭流程"""
+    mock_subprocess_run = mocker.patch("subprocess.run")
+
+    remove_type = "Bot"
+    mock_issue = MockIssue(
+        body=generate_issue_body_remove(type=remove_type), number=76
+    ).as_mock(mocker)
+
+    mock_issues_resp = mocker.MagicMock()
+    mock_issues_resp.parsed_data = mock_issue
+
+    mock_pulls_resp = mocker.MagicMock()
+    mock_pulls_resp.parsed_data = []
+
+    mock_comment = mocker.MagicMock()
+    mock_comment.body = "Bot: test"
+    mock_list_comments_resp = mocker.MagicMock()
+    mock_list_comments_resp.parsed_data = [mock_comment]
+
+    async with app.test_matcher() as ctx:
+        adapter, bot = get_github_bot(ctx)
+
+        event = get_mock_event(PullRequestClosed)
+        event.payload.pull_request.labels = get_pr_labels(["Remove", "Bot"])
+        event.payload.pull_request.merged = True
+
+        should_call_api(
+            ctx,
+            [
+                GitHubApi(
+                    api="rest.apps.async_get_repo_installation",
+                    result=mock_installation,
+                ),
+                GitHubApi(
+                    api="rest.issues.async_get",
+                    result=mock_issues_resp,
+                ),
+                GitHubApi(
+                    api="rest.issues.async_update",
+                    result=True,
+                ),
+                GitHubApi(
+                    api="rest.pulls.async_list",
+                    result=mock_pulls_resp,
+                ),
+            ],
+            snapshot(
+                {
+                    "rest.apps.async_get_repo_installation": {
+                        "owner": "he0119",
+                        "repo": "action-test",
+                    },
+                    "rest.issues.async_get": {
+                        "owner": "he0119",
+                        "repo": "action-test",
+                        "issue_number": 76,
+                    },
+                    "rest.issues.async_update": {
+                        "owner": "he0119",
+                        "repo": "action-test",
+                        "issue_number": 76,
+                        "state": "closed",
+                        "state_reason": "completed",
+                    },
+                    "rest.pulls.async_list": {
+                        "owner": "he0119",
+                        "repo": "action-test",
+                        "state": "open",
+                    },
+                }
+            ),
+        )
+        ctx.receive_event(bot, event)
+
+    # 测试 git 命令
+    mock_subprocess_run.assert_has_calls(
+        [
+            mocker.call(
+                ["git", "config", "--global", "safe.directory", "*"],
+                check=True,
+                capture_output=True,
+            ),
+            mocker.call(
+                ["git", "push", "origin", "--delete", "publish/issue76"],
+                check=True,
+                capture_output=True,
+            ),
+        ],  # type: ignore
+        any_order=True,
+    )
